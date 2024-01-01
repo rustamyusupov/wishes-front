3421ebcaf2cafef88a22c474e5d32445af400bf4
Rustam Yusupov <i@rstm.me>
2024-01-01 12:39:42 +0200

    config eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,14 +4,45 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:i18next/recommended',
+    'plugin:jsx-a11y/recommended',
     'plugin:prettier/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react/recommended',
     'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh', 'prettier'],
+  plugins: ['i18next', 'import', 'jsx-a11y', 'prettier', 'react-hooks', 'react-refresh', 'react'],
   rules: {
+    'import/no-unresolved': 'error',
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+        },
+        groups: ['builtin', 'external', 'internal', 'parent', ['index', 'sibling']],
+      },
+    ],
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      node: {
+        paths: ['./src'],
+      },
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+    react: {
+      version: 'detect',
+    },
   },
 };
