c3e8e4b72453821c1a26844f50c48953008ed225
Rustam Yusupov <i@rstm.me>
2024-01-01 13:13:08 +0200

    fix vite imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteTsconfigPaths()],
   server: {
     host: 'localhost',
     port: 3000,
