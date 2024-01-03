f3de96eed2ecc7445d7171697640f102aa48bc66
Rustam Yusupov <i@rstm.me>
2024-01-03 20:31:53 +0200

    add add link for home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,6 +2,8 @@ import { useTranslation } from 'react-i18next';
 import { useLoaderData } from 'react-router-dom';
 
 import { Category as ICategory } from 'api';
+import { Link } from 'components/Link';
+import Add from 'icons/add.svg?react';
 
 import { Wish } from './Wish';
 import css from './home.module.css';
@@ -12,7 +14,12 @@ export const Home = () => {
 
   return (
     <div className={css.home}>
-      <h1 className={css.title}>{t('title')}</h1>
+      <div className={css.header}>
+        <h1 className={css.title}>{t('title')}</h1>
+        <Link className={css.link} to="/wishes/add">
+          <Add className={css.add} width={24} height={24} />
+        </Link>
+      </div>
       {categories.map(category =>
         category.wishes?.length > 0 ? (
           <section className={css.category} key={category.id}>
