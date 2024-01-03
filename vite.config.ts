3f80d0de852e61e02d2f3f33a644b325fedc2bc5
Rustam Yusupov <i@rstm.me>
2024-01-03 20:31:12 +0200

    add svgr package

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   base: '/wishes',
-  plugins: [react(), viteTsconfigPaths()],
+  plugins: [react(), svgr(), viteTsconfigPaths()],
   server: {
     host: 'localhost',
     port: 3000,
