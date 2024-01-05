2515f5b41005f030f12183653844613c0c4a22a2
Rustam Yusupov <i@rstm.me>
2024-01-05 14:15:18 +0200

    add edit link for wishes

--- src/pages/Home/Wish.tsx
+++ src/pages/Home/Wish.tsx
@@ -1,7 +1,9 @@
 import { FC, ReactElement } from 'react';
 import { useTranslation } from 'react-i18next';
+import { Link as Route } from 'react-router-dom';
 
 import { Link } from 'components/Link';
+import Edit from 'icons/edit.svg?react';
 
 import { WishWithPrice } from './types';
 import css from './wish.module.css';
@@ -10,12 +12,15 @@ export const Wish: FC<WishWithPrice> = ({ id, link, name, prices, currency }): R
   const { t } = useTranslation(['common']);
 
   return (
-    <li className={css.category}>
+    <li className={css.wish}>
+      <Route className={css.link} to={`/wishes/${id}`}>
+        <Edit className={css.edit} width={26} height={26} />
+      </Route>
       <Link className={css.name} to={link}>
         {name}
       </Link>
       &nbsp;&mdash;&nbsp;
-      <Link className={css.price} to={`/wishes/${id}`}>
+      <Link className={css.price} to={``}>
         {t('currency', { currency: currency, val: prices.at(-1) ?? 0 })}
       </Link>
     </li>
