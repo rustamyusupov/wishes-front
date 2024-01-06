de76279a95f9ff075e9b00ab09b6acf0f6d43215
Rustam Yusupov <i@rstm.me>
2024-01-06 15:40:02 +0200

    change color for archive wishes

--- src/pages/Home/Wish.tsx
+++ src/pages/Home/Wish.tsx
@@ -1,3 +1,4 @@
+import cn from 'classnames';
 import { FC, ReactElement } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Link as Route } from 'react-router-dom';
@@ -8,11 +9,18 @@ import Edit from 'icons/edit.svg?react';
 import { WishWithPrice } from './types';
 import css from './wish.module.css';
 
-export const Wish: FC<WishWithPrice> = ({ id, link, name, prices, currency }): ReactElement => {
+export const Wish: FC<WishWithPrice> = ({
+  archive,
+  id,
+  link,
+  name,
+  prices,
+  currency,
+}): ReactElement => {
   const { t } = useTranslation(['common']);
 
   return (
-    <li className={css.wish}>
+    <li className={cn(css.wish, { [css.archive]: archive })}>
       <Route className={css.link} to={`/wishes/${id}`}>
         <Edit className={css.edit} width={26} height={26} />
       </Route>
