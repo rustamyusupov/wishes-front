ec72fceb8c4d2fbdea225100370308ef1fb97403
Rustam Yusupov <i@rstm.me>
2024-01-08 11:25:53 +0200

    fetch wishlist by user

--- src/api/loader.ts
+++ src/api/loader.ts
@@ -1,14 +1,14 @@
-import { LoaderFunction } from 'react-router-dom';
+import { LoaderFunction, LoaderFunctionArgs } from 'react-router-dom';
 
 import { fetchJSON } from './request';
 import { Category, Currency, Data, Wishlist } from './types';
 
-export const loader: LoaderFunction = async (): Promise<Data> => {
+export const loader: LoaderFunction = async ({ params }: LoaderFunctionArgs): Promise<Data> => {
   // TODO: request categories and currencies for wish page only
   const [categories, currencies, wishlist] = await Promise.all([
     await fetchJSON<Category[]>('/api/categories'),
     await fetchJSON<Currency[]>('/api/currencies'),
-    await fetchJSON<Wishlist>(`/api/wishlist`),
+    await fetchJSON<Wishlist>(`/api/wishlist?user=${params.user}`),
   ]);
 
   return { categories, currencies, wishlist };
